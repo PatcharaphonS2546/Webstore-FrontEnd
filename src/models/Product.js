4fb88e8ee0db1ff90db905ac5055e6b8848076ae
Lucilary <gofafafa1@gmail.com>
2023-12-22 08:38:45 +0700

    Before debugging POST

--- src/models/Product.js
+++ src/models/Product.js
@@ -12,7 +12,11 @@ const productSchema = new mongoose.Schema({
     },
     description: String,
     // Add more fields as needed
-});
+}, { timestamps: true }); // Add timestamps option
+
+// Make name and price not required during updates
+productSchema.path('name').required(false);
+productSchema.path('price').required(false);
 
 const Product = mongoose.model('Product', productSchema);
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -27,20 +27,71 @@ router.get('/products/:id', async (req, res) => {
 });
 
 // Create a new product
+// Create a new product
 router.post('/products', async (req, res) => {
-    const product = new Product({
+    const productData = {
         name: req.body.name,
         price: req.body.price,
         description: req.body.description,
         // Add more fields as needed
-    });
+    };
 
     try {
-        const newProduct = await product.save();
-        res.status(201).json(newProduct);
+        // Insert the product data into the MongoDB collection
+        const result = await Product.create(productData);
+
+        // Respond with the product data from the request
+        res.status(201).json({
+            _id: result._id,
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            createdAt: result.createdAt,
+            updatedAt: result.updatedAt,
+            // Add more fields as needed
+        });
     } catch (error) {
-        console.error('Error creating product:', error.message);
         res.status(400).json({ message: error.message });
+    }
+});
+
+
+
+// Update a product by ID (PATCH request)
+router.patch('/products/:id', async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const updateFields = req.body;
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            productId,
+            { $set: updateFields },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Delete a product by ID
+router.delete('/products/:id', async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const deletedProduct = await Product.findByIdAndRemove(productId);
+
+        if (!deletedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
